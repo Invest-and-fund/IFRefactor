11419e77b3ac1d3d3b2dbcdbf9cbd9beed1e58fa
James Coates <james.coates@drdoctor.co.uk>
2023-10-01 13:34:13 +0100

    Add design time factory

---
+++ AcmeStudios.ApiRefactor.DataAccess/AcmeStudiosContextDesignTimeFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AcmeStudios.ApiRefactor.DataAccess
+{
+    public class AcmeStudiosContextDesignTimeFactory : IDesignTimeDbContextFactory<AcmeStudiosContext>
+    {
+        public AcmeStudiosContext CreateDbContext(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = configBuilder.Build();
+            var optionsBuilder = new DbContextOptionsBuilder<AcmeStudiosContext>();
+            var connectionString = configuration.GetConnectionString("StudioConnection");
+
+            if (connectionString is not null)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+
+            return new AcmeStudiosContext(optionsBuilder.Options);
+        }
+    }
+}
